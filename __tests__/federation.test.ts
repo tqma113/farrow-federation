59c824aa5ecaedfa28d37127e727b2a354cb371d
Ma Tianqi <matianqi.tqma@bytedance.com>
2021-09-14 21:07:56 +0800

    test: add more test

--- __tests__/federation.test.ts
+++ __tests__/federation.test.ts
@@ -12,7 +12,7 @@ describe('farrow-federation', () => {
   http.route('/bar').use(bar)
   http.use(baz)
 
-  it('base usage', async () => {
+  it('Introspection', async () => {
     const federationService = await createFederationServices(
       [
         {
@@ -55,11 +55,180 @@ describe('farrow-federation', () => {
     const server = Http()
 
     server.use(federationService)
-
     const res = await supertest(server.handle).post('/').send({
       type: 'Introspection',
     })
 
     expect(res.body).toMatchSnapshot()
+  })
+
+  it('pass through', async () => {
+    const federationService = await createFederationServices(
+      [
+        {
+          url: '/foo',
+          namespace: 'foo',
+        },
+        {
+          url: '/bar',
+          namespace: 'bar',
+        },
+        {
+          url: '/greet',
+          namespace: 'greet',
+        },
+        {
+          url: '/todo',
+          namespace: 'todo',
+        },
+      ],
+      {
+        // @ts-ignore
+        fetch: async (input, init) => {
+          const getRes = () =>
+            new Promise<Response>((resolve) =>
+              supertest(http.server())
+                .post(input as string)
+                .send(JSON.parse(init!.body! as any))
+                .then(resolve),
+            )
+          const res = await getRes()
+          return {
+            json() {
+              return JSON.parse(res.text)
+            },
+          }
+        },
+      },
+    )
+
+    const server = Http()
+
+    server.use(federationService)
+    const res = await supertest(server.handle)
+      .post('/')
+      .send({
+        type: 'Single',
+        path: ['foo', 'getTodos'],
+        input: { id: 'foo' },
+      })
+
+    expect(res.body.type).toBe('ApiSuccessResponse')
+    expect(typeof res.body.output).toBe('object')
+    expect(Array.isArray(res.body.output.todos)).toBeTruthy()
+    expect(res.body.output.todos.length).toBe(3)
+  })
+
+  it('mutiple', async () => {
+    const federationService = await createFederationServices(
+      [
+        {
+          url: '/foo',
+          namespace: 'foo',
+        },
+        {
+          url: '/bar',
+          namespace: 'bar',
+        },
+        {
+          url: '/greet',
+          namespace: 'greet',
+        },
+        {
+          url: '/todo',
+          namespace: 'todo',
+        },
+      ],
+      {
+        // @ts-ignore
+        fetch: async (input, init) => {
+          const getRes = () =>
+            new Promise<Response>((resolve) =>
+              supertest(http.server())
+                .post(input as string)
+                .send(JSON.parse(init!.body! as any))
+                .then(resolve),
+            )
+          const res = await getRes()
+          return {
+            json() {
+              return JSON.parse(res.text)
+            },
+          }
+        },
+      },
+    )
+
+    const server = Http()
+
+    server.use(federationService)
+    const res = await supertest(server.handle)
+      .post('/')
+      .send({
+        type: 'Single',
+        path: ['bar', 'getTodos'],
+        input: { id: 'foo' },
+      })
+
+    expect(res.body.type).toBe('ApiSuccessResponse')
+    expect(typeof res.body.output).toBe('object')
+    expect(Array.isArray(res.body.output.todos)).toBeTruthy()
+    expect(res.body.output.todos.length).toBe(3)
+  })
+
+  it('nest', async () => {
+    const federationService = await createFederationServices(
+      [
+        {
+          url: '/foo',
+          namespace: 'foo',
+        },
+        {
+          url: '/bar',
+          namespace: 'bar',
+        },
+        {
+          url: '/greet',
+          namespace: 'greet',
+        },
+        {
+          url: '/todo',
+          namespace: 'todo',
+        },
+      ],
+      {
+        // @ts-ignore
+        fetch: async (input, init) => {
+          const getRes = () =>
+            new Promise<Response>((resolve) =>
+              supertest(http.server())
+                .post(input as string)
+                .send(JSON.parse(init!.body! as any))
+                .then(resolve),
+            )
+          const res = await getRes()
+          return {
+            json() {
+              return JSON.parse(res.text)
+            },
+          }
+        },
+      },
+    )
+
+    const server = Http()
+
+    server.use(federationService)
+    const res = await supertest(server.handle)
+      .post('/')
+      .send({
+        type: 'Single',
+        path: ['greet', 'greet'],
+        input: { name: 'farrow' },
+      })
+
+    expect(res.body.type).toBe('ApiSuccessResponse')
+    expect(typeof res.body.output).toBe('object')
+    expect(res.body.output.greet).toBe('Hello farrow!')
   })
 })
